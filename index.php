b72f62eb416cc67ca9427681d2eff81dd813a399
Kaiyu Zheng <kaiyutony@gmail.com>
2015-02-21 18:43:02 -0800

    Adding an index page, where the demo is put

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+// This file will be the index page - one that has content showing on it
+require_once "view/card_view.php";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Demo</title>
+    <link rel="stylesheet" href="css/card.css">
+  </head>
+  <body>
+    <div class="whole-wrapper">
+      <h1>Demo:</h1>
+      <div class="main-content">
+	<?php 
+	
+	?>
+	<script src="script/card.js"></script>
+      </div>
+    </div>
+  </body>
+</html>
+<?php  
+
+?>
